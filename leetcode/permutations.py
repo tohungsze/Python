3885968d5b5a9c173df34572398419d280cb91c7
To Hung Sze <tohungsze.acton@gmail.com>
2020-03-22 16:39:41 -0400

    added permutations.py

---
+++ leetcode/permutations.py
@@ -0,0 +1,48 @@
+"""Given a collection of distinct integers, return all possible permutations.
+Note:
+will eat up memory if input is long
+"""
+
+
+def all_perms(elements):
+    if len(elements) <=1:
+        yield elements
+    else:
+        for perm in all_perms(elements[1:]):
+            for i in range(len(elements)):
+                # nb elements[0:1] works in both string and list contexts
+                yield elements[0:1] + perm[:i] + perm[i:]
+
+
+# test
+result = []
+for i in (all_perms([1, 2, 3])):     # [[1, 2], [2, 1]]
+    result.append(i)
+print(result)
+# [
+#   [1,2,3],
+#   [1,3,2],
+#   [2,1,3],
+#   [2,3,1],
+#   [3,1,2],
+#   [3,2,1]
+# ]
+# permutation([1,2,3])
+
+# alternate solution based on itertool
+import itertools as it
+
+
+def permutations(iterable, r=None):
+    pool = tuple(iterable)      # yield tuple instead of list to contrast the list from above
+    n = len(pool)               # can change to tuple if needed
+    r = n if r is None else r
+    for indices in it.product(range(n), repeat=r):
+        if len(set(indices)) == r:
+            yield tuple(pool[j] for j in indices)
+
+
+result = []
+for i in permutations([1, 2, 3]):
+    result.append(i)
+print(result)
